4fd2660f8a2c079c39c7ce560821d705b96498ce
Nathanloupy <94622983+Nathanloupy@users.noreply.github.com>
2025-08-08 12:04:14 +0200

    fix: removing calls to printf

--- src/display_symbols.c
+++ src/display_symbols.c
@@ -1,5 +1,36 @@
 #include "ft_nm.h"
-#include <stdio.h>
+
+/**
+ * @brief Print a hexadecimal value in lower case
+ * 
+ * @param value The value to print
+ * @param is8byte Whether the value is 8 or 16 bytes
+ */
+static void print_hex_lower(unsigned long value, char is16byte)
+{
+	char		buffer16[16];
+	char		buffer8[8];
+	const char	*base = "0123456789abcdef";
+
+	if (is16byte)
+	{
+		for (int i = 15; i >= 0; --i)
+		{
+			buffer16[i] = base[value & 0xF];
+			value >>= 4;
+		}
+		write(1, buffer16, 16);
+	}
+	else
+	{
+		for (int i = 7; i >= 0; --i)
+		{
+			buffer8[i] = base[value & 0xF];
+			value >>= 4;
+		}
+		write(1, buffer8, 8);
+	}
+}
 
 /**
  * @brief Display a single symbol in the standard nm format
@@ -10,15 +41,14 @@
 static void	display_single_symbol(t_symbol *symbol, t_file *file)
 {
 	if (symbol->type_char == 'U' || symbol->type_char == 'w' || symbol->type_char == 'v')
-		printf("                ");
+		ft_putstr_fd("                ", 1);
 	else
-	{
-		if (file->elf_data.is_64bit)
-			printf("%016lx", symbol->value);
-		else
-			printf("%08lx", symbol->value);
-	}
-	printf(" %c %s\n", symbol->type_char, symbol->name);
+		print_hex_lower(symbol->value, !file->elf_data.is_64bit);
+	ft_putstr_fd(" ", 1);
+	ft_putchar_fd(symbol->type_char, 1);
+	ft_putstr_fd(" ", 1);
+	ft_putstr_fd(symbol->name, 1);
+	ft_putstr_fd("\n", 1);
 }
 
 /**
